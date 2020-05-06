ba7b01cba202a881aea7383b6386401c8bafe18a
Luke Hansford <mail@lukehansford.me>
2020-05-06 21:29:56 +0200

    Remove some unnecessary parts

--- README.md
+++
@@ -1,4 +0,0 @@
-# typescript-script-template
-
-I find myself writing scripts in Typescript from time to time, so this is just a simple template to
-get started quicker.
--- config/rollup.config.js
+++ config/rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
 import json from '@rollup/plugin-json';
 
 import pkg from '../package.json';
@@ -22,7 +21,6 @@ export default {
     resolve({ extensions, preferBuiltins: true }),
     json(),
     commonjs(),
-    preserveShebangs(),
     typescript({ emitDeclarationOnly: true }),
     babel({
       exclude: 'node_modules/**',
